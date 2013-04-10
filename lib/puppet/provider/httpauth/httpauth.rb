639ae64d320fb2a253a21fe0da959a7eb8064a99
Thomas Van Doren <thomas.vandoren@gmail.com>
2013-04-10 02:01:27 -0700

    Rough draft implementation of owner/group/mode support in provider.
    Adds some new helpers to the httpauth provider for checking and setting file permissiongs (mode), uid (owner), and gid (group).

--- lib/puppet/provider/httpauth/httpauth.rb
+++ lib/puppet/provider/httpauth/httpauth.rb
@@ -10,7 +10,8 @@ Puppet::Type.type(:httpauth).provide(:httpauth) do
     def create
         # Create a user in the file we opened in the mech method
         @htauth.set_passwd(resource[:realm], resource[:name], resource[:password])
-        @htauth.flush 
+        @htauth.flush
+        set_file_permissions()
     end
  
     def destroy
@@ -21,20 +22,25 @@ Puppet::Type.type(:httpauth).provide(:httpauth) do
  
     def exists?
         # Check if the file exists at all
-        if File.exists?(resource[:file])
+        if not File.exists?(resource[:file])
+            # If the file doesn't exist then create it
+            File.new(resource[:file], "w").close
+            mech(resource[:file])
+            return false
+        else
             # If it does exist open the file
             mech(resource[:file])
 
             # Check if the user exists in the file
             cp = @htauth.get_passwd(resource[:realm], resource[:name], false)
             return false if cp == nil
+
             # Check if the current password matches the proposed password
-            return check_passwd(resource[:realm], resource[:name], resource[:password], cp)
-        else
-            # If the file doesn't exist then create it
-            File.new(resource[:file], "w", resource[:mode].to_i(8)).chown(Etc.getpwnam(resource[:owner]).uid, Etc.getgrnam(resource[:group]).gid)
-            mech(resource[:file])
-            return false
+            if not check_passwd(resource[:realm], resource[:name], resource[:password], cp)
+              return false
+            # Check that the file has the correct permissions.
+            else
+              return check_file()
         end
     end
 
@@ -47,6 +53,16 @@ Puppet::Type.type(:httpauth).provide(:httpauth) do
         end
     end
 
+    # Check file permissions (mode), uid (owner), and gid (group)
+    def check_file()
+        if not File.exists?(resource[:file])
+          return false
+        fstat = File.stat(resource[:file])
+        return (fstat.mode.to_s(8).end_with?(resource[:mode]) and
+                fstat.uid == Etc.getpwnam(resource[:owner]).uid and
+                fstat.gid == Etc.getgrnam(resource[:group]).gid)
+    end
+
     # Check password matches
     def check_passwd(realm, user, password, cp)
         if resource[:mechanism] == :digest
@@ -54,6 +70,15 @@ Puppet::Type.type(:httpauth).provide(:httpauth) do
         elsif resource[:mechanism] == :basic
             # Can't ask webbrick as it uses a random seed
             password.crypt(cp[0,2]) == cp
+        end
+    end
+
+    # Ensure file permissions (mode), uid (owner), and gid (group) are set.
+    # Caller is responsible for determining the need for this.
+    def set_file_permissions()
+        File.open(resource[:file], "r") do |fp|
+            fp.chmod(resource[:mode].to_i(8))
+            fp.chown(Etc.getpwnam(resource[:owner]).uid, Etc.getgrname(resource[:group]).gid)
         end
     end
 end
